87c5706ae9fb6c8bd250f3b60c78293858f1481d
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-11-10 03:40:58 +0530

    Header Changes

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,24 +1,36 @@
+import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
-import style from './Header.module.css';
+import styles from './Header.module.css';
 import logo from './ASK.svg';
 
-const Header = () => {
+const Header = ({ transparent }) => {
+  console.log(transparent);
   return (
-    <div className={style.headerContainer}>
-      <div className={style.logoContainer}>
+    <div
+      className={`${styles.headerContainer} 
+      ${!transparent && styles.addColor}`}>
+      <div className={styles.logoContainer}>
         <img src={logo} alt='logo' /> BnB
       </div>
 
-      <div className={style.buttonContainer}>
-        <Button basic color='black' className={style.buttonPadding}>
+      <div className={styles.buttonContainer}>
+        <Button basic color='black' className={styles.buttonPadding}>
           Login
         </Button>
-        <Button className={style.buttonPadding} secondary>
+        <Button className={styles.buttonPadding} secondary>
           Sign Up
         </Button>
       </div>
     </div>
   );
+};
+
+Header.defaultProps = {
+  transparent: false,
+};
+
+Header.propType = {
+  transparent: PropTypes.bool.isRequired,
 };
 
 export default Header;

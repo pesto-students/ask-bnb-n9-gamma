4835119097ec98db75b3ff09b2e00709c6092b61
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-11-10 01:34:01 +0530

    Rooms detail page UI (non-functional)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import HotelList from './pages/HotelList/HotelList';
+import HotelDetail from './pages/HotelDetails/HotelDetail';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/list' component={HotelList} />
-          <Route exact path='/hotel/:hotelId' component={HotelList} />
+          <Route exact path='/hotel/:hotelId' component={HotelDetail} />
         </Switch>
       </Router>
     </div>

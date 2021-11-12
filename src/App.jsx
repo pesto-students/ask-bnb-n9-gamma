7f5018893be135f7c2e6dbb32822e3bec68e1a5f
Shahid <shahidpt0982219@gmail.com>
2021-11-12 20:02:14 +0530

    Signin and Signup added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import HotelList from './pages/HotelList/HotelList';
+import Auth from './pages/Auth/Auth';
 
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/list' component={HotelList} />
-          <Route exact path='/hotel/:hotelId' component={HotelList} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/list" component={HotelList} />
+          <Route exact path="/hotel/:hotelId" component={HotelList} />
+          <Route exact path="/auth" component={Auth} />
         </Switch>
       </Router>
     </div>

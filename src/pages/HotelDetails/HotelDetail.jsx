86f139cdb5b0d0b4b36dc38fa9899539898c9b1d
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-11-19 04:13:40 +0530

    Selecting and blocking room functionalities

--- src/pages/HotelDetails/HotelDetail.jsx
+++ src/pages/HotelDetails/HotelDetail.jsx
@@ -1,59 +1,186 @@
-import { FaMinus, FaPlus, FaRegTrashAlt } from 'react-icons/fa';
+import { FaPlus, FaRegTrashAlt } from 'react-icons/fa';
 import { Button } from 'semantic-ui-react';
 import Footer from '../shared/Footer';
 import Header from '../shared/Header';
 import styles from './HotelDetail.module.css';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { blockRooms } from '../../actions/hotelAction';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
 
-const HotelDetail = () => {
-  const data = {
-    hotelDetail: '1 BHK Service Apartment 1',
-    note: 'Fully furnished apartment in HRS Layout',
-    amnities: [
-      '2 guest',
-      '1 Bed',
-      'Studio',
-      '2 Bathrooms',
-      'WiFi',
-      'Kitchen',
-      'Free Parking',
-    ],
-    reviews: '11',
-    ratings: '4.5',
-    averagePrice: '1000',
-    _id: 1,
-    image:
-      'https://media.istockphoto.com/photos/downtown-cleveland-hotel-entrance-and-waiting-taxi-cab-picture-id472899538?b=1&k=20&m=472899538&s=170667a&w=0&h=oGDM26vWKgcKA3ARp2da-H4St2dMEhJg23TTBeJgPDE=',
-    availableRooms: 3,
-    maxOccupancy: 2,
+const moment = extendMoment(Moment);
+
+const HotelDetail = ({
+  currentHotel,
+  filter: { startDate, endDate, guests },
+  blockRooms,
+  history,
+}) => {
+  const {
+    hotel_id,
+    room_collection: { standard },
+  } = currentHotel;
+  console.log(currentHotel);
+
+  const [roomCount, setRoomCount] = useState(1);
+  const [minRoom] = useState(Math.ceil(guests / 2));
+  const [maxRoomAvailable, setMaxRoomAvailable] = useState(0);
+  const [roomsSelected, setRoomsSelected] = useState([]);
+
+  useEffect(() => {
+    setRoomCount(minRoom);
+    getMinRoomAvailed();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [minRoom]);
+
+  const bookNewRooms = () => {
+    blockRooms(hotel_id, roomsSelected.slice(0, roomCount), startDate, endDate);
+    // history.push('')
   };
 
-  const getRoomDetails = () => (
+  const getMinRoomAvailed = () => {
+    let roomArray = getRoomBasedOnDate();
+    setRoomsSelected(roomArray);
+    console.log(roomsSelected);
+    let totalRoomBasedOnDate = roomArray.length;
+    let totalRoomBasedOnGuest = guests;
+    let maxRoom =
+      totalRoomBasedOnDate < totalRoomBasedOnGuest
+        ? totalRoomBasedOnDate
+        : totalRoomBasedOnGuest;
+    console.log('maxRoom', maxRoom);
+    setMaxRoomAvailable(maxRoom);
+  };
+
+  const getRoomBasedOnDate = () => {
+    const bookingRange = moment.range(startDate, endDate);
+
+    let rooms_selected = [];
+    standard.forEach(room => {
+      if (
+        !room.bookingDurations.some(durationArray =>
+          bookingRange.overlaps(
+            moment.range(durationArray[0], durationArray[1])
+          )
+        )
+      ) {
+        rooms_selected.push({
+          type: 'standard',
+          room_id: room.room_id,
+          price: room.base_price,
+        });
+      }
+    });
+    return rooms_selected;
+  };
+
+  const getRoomDetails = room_data => (
     <div>
-      <div className={styles.roomSelection}>
-        <div className={styles.guestDetailRow}>
-          <span className={styles.roomNum}>Room 1</span>
-          <div className={styles.addGuestRow}>
-            <span className={`${styles.removeGuest} ${styles.buttonCircle}`}>
-              <FaPlus />
-            </span>
-            <span className={styles.guestCount}>1 Guest</span>
-            <span className={`${styles.addGuest} ${styles.buttonCircle}`}>
-              <FaMinus />
-            </span>
+      {roomsSelected.slice(0, roomCount).map((room, index) => {
+        return (
+          <div key={index} className={styles.roomSelection}>
+            <div className={styles.guestDetailRow}>
+              <span className={styles.roomNum}>Room {index + 1}</span>
+              <div className={styles.addGuestRow}>
+                <span className={styles.guestCount}>
+                  Maximum {room_data.room_type[0]}
+                </span>
+              </div>
+              {!(
+                !index ||
+                index + 1 !== roomCount ||
+                index + 1 === minRoom
+              ) && (
+                <span
+                  className={styles.removeLink}
+                  onClick={() => setRoomCount(roomCount - 1)}>
+                  <FaRegTrashAlt size='100%' />
+                </span>
+              )}
+            </div>
           </div>
-          <span className={styles.removeLink}>
-            <FaRegTrashAlt size='100%' />
-          </span>
+        );
+      })}
+      {maxRoomAvailable > roomCount && (
+        <div
+          className={styles.addRoom}
+          onClick={() => setRoomCount(roomCount + 1)}>
+          <FaPlus size='10' style={{ padding: '0 2px 0 0' }} />{' '}
+          <span>Add Rooms</span>
         </div>
-      </div>
-      <div className={styles.addRoom}>
-        <FaPlus size='10' style={{ padding: '0 2px 0 0' }} />{' '}
-        <span>Add Rooms</span>
-      </div>
+      )}
     </div>
   );
 
+  const getImageGrid = () => (
+    <>
+      {currentHotel.images.slice(0, 4).map(item => {
+        if (item) return <img src={item} alt='' />;
+        else return null;
+      })}
+    </>
+  );
+
+  const getRoomType = () => {
+    return currentHotel?.room_collection?.standard[0].room_type
+      .join(' - ')
+      .toString();
+  };
+
+  const getAmenityList = array => (
+    <ul>
+      {array.room_type.map(item => (
+        <li>{item}</li>
+      ))}
+    </ul>
+  );
+
+  const getRoomDetail = (data, room_type) => {
+    const room_data = data.room_collection[`${room_type}`][0];
+    let nights = moment.range(startDate, endDate).diff('days');
+    return (
+      <div
+        className={`${styles.roomCustomizationContainer}
+          ${styles[`${room_type}`]}`}>
+        <div className={styles.leftContainer}>
+          <img src={data.images[0]} alt='' className={styles.roomImage} />
+          <div className={styles.amenitiesColumn}>
+            {getAmenityList(room_data)}
+          </div>
+        </div>
+        <div className={styles.rightContainer}>
+          {getRoomDetails(room_data)}
+          <div className={styles.verticalLine}></div>
+          <div className={styles.priceWrapper}>
+            <div className={styles.priceContainer}>
+              <div className={styles.totalPrice}>
+                Rs. {room_data.base_price}
+              </div>
+              <div className={styles.priceDescription}>
+                for maximum {room_data.room_type[0]}
+                s, 1 night
+              </div>
+            </div>
+            <div className={styles.flexBox}>
+              {/* <Link to='/'> */}
+              <Button
+                color='yellow'
+                className={styles.bookButton}
+                onClick={() => bookNewRooms()}>
+                Book (Rs. {room_data.base_price * nights * roomCount})
+              </Button>
+              {/* </Link> */}
+              <div className={styles.priceDescription}>
+                for {roomCount} room(s), {nights} night
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <>
       <div>
@@ -62,55 +189,19 @@ const HotelDetail = () => {
       <div className={styles.detailWrapper}>
         <div className={styles.imageContainer}>
           <div className={styles.mainImageContainer}>
-            <img src={data.image} alt='' />
+            <img src={currentHotel.indexImage} alt='' />
           </div>
           <div className={styles.additionalImageContainer}>
-            <img src={data.image} alt='' />
-            <img src={data.image} alt='' />
-            <img src={data.image} alt='' />
-            <img src={data.image} alt='' />
+            {getImageGrid()}
           </div>
         </div>
         <div className={styles.decriptionContainer}>
-          <div className={styles.heading}>
-            Fully furnished apartment in HRS Layout
-          </div>
-          <div className={styles.minDetail}>
-            2 guest - 1 Bed - Studio - 2 Bathrooms
-          </div>
+          <div className={styles.heading}>{currentHotel.description}</div>
+          <div className={styles.minDetail}>{getRoomType()}</div>
         </div>
-        <div className={styles.roomCustomizationContainer}>
-          <div className={styles.leftContainer}>
-            <img src={data.image} alt='' className={styles.roomImage} />
-            <div className={styles.amenitiesColumn}>
-              <ul>
-                <li>Fits 3</li>
-                <li>Fits 3</li>
-                <li>Fits 3</li>
-                <li>Fits 3</li>
-              </ul>
-            </div>
-          </div>
-          <div className={styles.rightContainer}>
-            {getRoomDetails()}
-            <div className={styles.verticalLine}></div>
-            <div className={styles.priceWrapper}>
-              <div className={styles.priceContainer}>
-                <div className={styles.totalPrice}>Rs. 1234</div>
-                <div className={styles.priceDescription}>
-                  for 2 guest, 1 night
-                </div>
-              </div>
-              <div className={styles.flexBox}>
-                <Link to='/'>
-                  <Button color='yellow' className={styles.bookButton}>
-                    Book
-                  </Button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        {getRoomDetail(currentHotel, 'standard')}
+        {/* {currentHotel?.room_collection?.delux &&
+          getRoomDetail(currentHotel, 'delux')} */}
         <div className='amenityContainer'>
           <div className='amenityHeader'></div>
           <div className='availedAmenities'></div>
@@ -121,4 +212,14 @@ const HotelDetail = () => {
   );
 };
 
-export default HotelDetail;
+HotelDetail.propTypes = {
+  currentHotel: PropTypes.object.isRequired,
+  blockRooms: PropTypes.func.isRequired,
+};
+
+const mapStateToProp = state => ({
+  currentHotel: state.hotel.current,
+  filter: state.hotel.filter,
+});
+
+export default connect(mapStateToProp, { blockRooms })(HotelDetail);
--- src/pages/HotelList/HotelList.jsx
+++ src/pages/HotelList/HotelList.jsx
@@ -3,15 +3,48 @@ import Footer from '../shared/Footer';
 import styles from './HotelList.module.css';
 import HotelListItem from './HotelListItem';
 import { connect } from 'react-redux';
+import { getHotels } from '../../actions/hotelAction';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
 
-const HotelList = ({ hotelList }) => {
-  // console.log(hotelList);
+const moment = extendMoment(Moment);
+
+const HotelList = ({ hotel: { filter, hotelList }, getHotels, history }) => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  // const [range1, setRange1] = useState(null);
+  const range1 = moment.range(startDate, endDate);
+
+  useEffect(() => {
+    getHotels('Bangalore');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    setStartDate(filter.startDate);
+    setEndDate(filter.endDate);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filter]);
+
+  // useEffect(() => {
+  //   setRange1(moment.range(startDate, endDate));
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [startDate, endDate]);
+
   const getIndividualItems = () => {
     return hotelList.map(item => (
-      <HotelListItem key={item._id} styles={styles} data={item} />
+      <HotelListItem
+        key={item._id}
+        styles={styles}
+        data={item}
+        history={history}
+      />
     ));
   };
+  // useEffect(() => {
+  // }, [hotelList])
 
   return (
     <>
@@ -19,7 +52,9 @@ const HotelList = ({ hotelList }) => {
         <Header color='#2E2E2E' />
         <div className={styles.listSummary}>
           <div className={styles.filterDetails}>
-            100+ stays | 18th October to 21st October | 2 guests
+            100+ stays | {moment(startDate).format('Do MMMM')} to{' '}
+            {moment(endDate).format('Do MMMM')} | {range1.diff('days')} days |{' '}
+            {filter.guests} guests
           </div>
           <div className={styles.locationDetails}>Stay in Bangalore</div>
         </div>
@@ -32,10 +67,11 @@ const HotelList = ({ hotelList }) => {
 
 HotelList.propType = {
   hotel: PropTypes.array.isRequired,
+  getHotels: PropTypes.func.isRequired,
 };
 
 const mapStateToProp = state => ({
-  hotelList: state.hotel.hotelList,
+  hotel: state.hotel,
 });
 
-export default connect(mapStateToProp, {})(HotelList);
+export default connect(mapStateToProp, { getHotels })(HotelList);
